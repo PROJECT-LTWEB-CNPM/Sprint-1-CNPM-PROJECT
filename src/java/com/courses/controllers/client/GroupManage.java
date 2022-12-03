0d1e18a6c523c3388bc45949b7ea1e7031a26daf
PhamNguyenNhutTruong <nguyennhuttruong9972@gmail.com>
2022-12-03 21:17:50 +0700

    add code in DAO, Services and UI in addMemberForm.jsp

--- src/java/com/courses/controllers/client/GroupManage.java
+++ src/java/com/courses/controllers/client/GroupManage.java
@@ -18,22 +18,7 @@ public class GroupManage extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "/pages/client/student/groupManage.jsp";
-		Student student = new Student();
-		
-		RegisterGroupService registerGroupService = new RegisterGroupService(request, response);
-		StudentService studentService = new StudentService(request, response);
-		GroupService groupService = new GroupService(request, response);
-//		String studentId: Biến dùng để truyền thông tin của account đăng nhập(Lấy mã sinh viên hay mã định danh thay thế cho "ST00000002": thông tin được fix cứng)
-		String studentId = studentService.getStudentByPersonToLoginData().getStudentId();
-		
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("studentId", studentId);
-		
-		List<Student> students = new ArrayList<Student>();
-		List<GroupStudent> groupStudents = new ArrayList<GroupStudent>();
-		
-		students = studentService.checkStudentAndGroup(map);
+		request.getRequestDispatcher(url).forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
