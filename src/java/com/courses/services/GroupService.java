bae0e97d63ca973320a974089559a4ca4e3578b5
DODUONGTHAITUAN <doduongthaituan201102@gmail.com>
2022-12-06 21:25:46 +0700

    change logic addMemberToGroup

--- src/java/com/courses/services/GroupService.java
+++ src/java/com/courses/services/GroupService.java
@@ -140,4 +140,66 @@ public class GroupService extends SuperService {
 			return "member";
 		}
 	}
+
+
+	public void addMemberToGroup() throws ServletException, IOException {
+//		Kiểm tra xem người yêu cầu thêm thành viên có phải là nhóm trưởng hay không
+//		Kiểm tra mã sinh viên vừa nhập vào đã có nhóm hay chưa. Nếu chưa mới cho thêm vào nhóm
+//		Kiểm tra tính hợp lệ của id được nhập vào.
+		String url = "/student/group-manage";
+		GroupService groupService = new GroupService(request, response);
+		StudentService studentService = new StudentService(request, response);
+		JoinGroupService joinGroupService = new JoinGroupService(request, response);
+		
+		StudentDAO studentDAO = new StudentDAO(); 
+		GroupStudentDAO groupStudentDAO = new GroupStudentDAO();
+		
+		Student student = new Student();
+		GroupStudent groupStudent = new GroupStudent();
+
+		
+		List<GroupStudent> groupStudents = new ArrayList<GroupStudent>();
+		List<Student> students = new ArrayList<Student>();
+// Lấy dữ đối tượng đăng nhập vào thôgn qua login	
+		String leaderId = studentService.getStudentByPersonToLoginData().getStudentId();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+//		Kiểm tra xe người đăng nhập vào có phải là trưởng nhóm hay ko
+//		"ST00000002": sẽ được lấy từ login trả về
+		map.put("leaderId", leaderId);
+		groupStudents = groupService.checkLeader(map);
+		
+		String student_id = request.getParameter("studentId");
+		System.out.println("==============="+ student_id +"===============");
+		map.clear();
+		map.put("studentId", student_id);
+		students = studentService.checkStudentAndGroup(map);
+//		Kiểm tra xe người đăng nhập vào có phải là trưởng nhóm hay ko và có phải sinh viên hay không
+		if(groupStudents.size() > 0 && students.size() > 0) {
+			this.request.setAttribute("message", "Cần chọn đề tài trước khi thêm thành viên vào nhóm");
+		} else {
+//			Nếu sinh viên đăng nhập là nhóm trưởng thì mới có quyền thêm sinh viên mới vào nhóm.
+			groupStudents = groupService.checkRole(leaderId);
+			if (groupStudents.size() > 0) {
+				student = students.get(0);
+				groupStudent = groupStudents.get(0);
+				if (groupStudent.getTopic().getMaxMoMember() > groupStudent.getCurrentNumber()) {
+					groupStudent.setCurrentNumber(groupStudent.getCurrentNumber() + 1);
+					student.setGroupstudent(groupStudents.get(0));
+					groupStudentDAO.update(groupStudent);
+					studentDAO.update(student);	
+//			Xóa 1 sinh viên ra khỏi hàng chò xin vào nhóm khi xin viên đó được thêm vào nhóm nào đó
+//					joinGroupService.deleteRequestJoinGroup(student.getStudentId(), groupStudent.getGroupId());
+					joinGroupService.deleteAllRequestJoinGroupsRelatedToStudentId(student_id);
+					this.request.setAttribute("message", "Thêm thành viên thành công");
+					
+				} else {
+					this.request.setAttribute("message", "Số lượng thành viên của nhóm vượt quá số lượng thành viên cho phép của đề tài");
+				}
+			} else {
+				this.request.setAttribute("message", "Chỉ có trưởng nhóm mới được thêm thành viên");
+			}
+		}
+		this.request.getRequestDispatcher(url).forward(request, response);
+	}
 }
