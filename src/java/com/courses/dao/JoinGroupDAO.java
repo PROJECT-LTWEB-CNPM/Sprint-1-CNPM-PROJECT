68ddaa4bdd1a8685d83e32947644d3aea41d6cd4
PhamNguyenNhutTruong <nguyennhuttruong9972@gmail.com>
2022-12-07 18:50:20 +0700

    handle conflict

--- src/java/com/courses/dao/JoinGroupDAO.java
+++ src/java/com/courses/dao/JoinGroupDAO.java
@@ -39,15 +39,9 @@ public class JoinGroupDAO extends JpaDAO<JoinGroup> implements GenericDAO<JoinGr
 	@Override
 	public List<JoinGroup> findWithNamedQuery(String queryName, Map<String, Object> parameters) {
 		return super.findWithNamedQuery(queryName, parameters);
-<<<<<<< HEAD
 	}	
 
-	
-=======
-	}
 
-
->>>>>>> e3bcbe2c9c1135d48e1454a9f886a75d4ac43407
 	@Override
 	public List<JoinGroup> pagination(int currentPage, int pageSize) {
 		String queryName = "JoinGroup.findAll";
